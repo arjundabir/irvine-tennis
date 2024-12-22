6ff58853e25725b6a90af469f7413fdbad45155a
Arjun Dabir <arjunadabir@gmail.com>
2024-12-21 17:43:04 -0800

    added create-account folder, installed react-hook-form

---
+++ app/create-account/page.tsx
@@ -0,0 +1,12 @@
+import UserForm from "@/components/user-form";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="container mx-auto">
+      <UserForm />;
+    </div>
+  );
+};
+
+export default page;
---
+++ components/user-form.tsx
@@ -0,0 +1,138 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+export default function UserForm() {
+  const router = useRouter();
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+  });
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const name: string = formData.firstName + " " + formData.lastName;
+    const formDataUpdated = {
+      name,
+      email: formData.email,
+      phone: formData.phone,
+    };
+    const response = await fetch("/api/create-account", {
+      method: "POST",
+      body: JSON.stringify(formDataUpdated),
+    });
+    const data = await response.json();
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="space-y-12">
+        <div className="border-b border-gray-900/10 pb-12">
+          <h2 className="text-base/7 font-semibold text-gray-900">
+            Create Account
+          </h2>
+          <p className="mt-1 text-sm/6 text-gray-600">
+            Join our community of tennis players and find available courts
+            today.
+          </p>
+
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="first-name"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                First name
+              </label>
+              <div className="mt-2">
+                <input
+                  required
+                  id="first-name"
+                  name="first-name"
+                  type="text"
+                  autoComplete="given-name"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="last-name"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Last name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="last-name"
+                  name="last-name"
+                  type="text"
+                  autoComplete="family-name"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="email"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  required
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="tel"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Phone number
+              </label>
+              <div className="mt-2">
+                <input
+                  required
+                  id="tel"
+                  name="tel"
+                  type="tel"
+                  pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                  autoComplete="tel"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-6 flex items-center justify-end gap-x-6">
+        <button
+          type="button"
+          className="text-sm/6 font-semibold text-gray-900"
+          onClick={() => router.back()}
+        >
+          Back
+        </button>
+        <button
+          type="submit"
+          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Create Account
+        </button>
+      </div>
+    </form>
+  );
+}
